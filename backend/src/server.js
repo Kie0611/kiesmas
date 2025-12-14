4eb7770ba519b02dc5391cf1316110518a443f24
Kean <wkeanphilip@gmail.com>
2025-12-14 16:55:23 +0800

    Update

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,6 +3,7 @@ import messageRoutes from './routes/messageRoutes.js'
 import { connectDB } from './config/db.js';
 import dotenv from "dotenv"
 import cors from 'cors';
+import path from "path"
 
 dotenv.config()
 
@@ -10,6 +11,7 @@ connectDB();
 
 const app = express()
 const PORT = process.env.PORT
+const __dirname = path.resolve();
 
 app.use(express.json())
 app.use(cors({
@@ -18,6 +20,14 @@ app.use(cors({
 
 
 app.use("/api/message", messageRoutes)
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 connectDB().then(() => {
   const server = app.listen(PORT, () => {
